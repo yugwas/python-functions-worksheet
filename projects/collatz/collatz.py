be2d2c666aa74934a59898f726ab2d5a4ffc6b18
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-10-06 08:50:43 +0000

    Working on collatz function, finished minimum function.

--- projects/collatz/collatz.py
+++ projects/collatz/collatz.py
@@ -1,16 +1,31 @@
+
 def collatz(number):
+    for i in range(number):
+        if i % 2 == 0:
+            number = number % 2
+            return(number)
+        else:
+            number = 3 * number + 1
+            return(number)
 
+        
 
+#The Collatz Sequence  
 
+#Write a function named collatz() that has one parameter named number. If number is even, then collatz() should print number // 2 and return this value. If number is odd, then collatz() should print and return 3 * number + 1.
+
+#Then write code that lets the user type in an integer and that keeps calling collatz() on that number until the function returns the value 1. (Amazingly enough, this sequence actually works for any integer—sooner or later, using this sequence, you’ll arrive at 1! Even mathematicians aren’t sure why. Your program is exploring what’s called the Collatz sequence, sometimes called “the simplest impossible math problem.”)
+
+#Remember to convert the return value from input() to an integer with the int() function; otherwise, it will be a string value.
+
+#####################################
 
 # def user_input(number):
 
 
-
-
-
 # Uncomment this code to test your function
-# print(collatz(3))
+print(collatz(3))
+
 
 '''The output of this program could look something like this:
 
--- projects/min3/min3.py
+++ projects/min3/min3.py
@@ -1,18 +1,26 @@
 def min3 (num1, num2, num3):
-
-
-
-
-
-
-
+    if num1 <= num2:
+        if num1 <= num3:
+            return num1
+        else:
+            return num3
+    else:
+        if num2 <= num3:
+            return num2
+        else:
+            return num3
+    
+    
 # Uncomment this code to test your function
 
-# print(min3(4, 7, 5))  
-# print(min3(4, 5, 5))  
-# print(min3(4, 4, 4))  
-# print(min3(-2, -6, -100))  
-# print(min3("Z", "B", "A"))  
+print(min3(4, 7, 5))  
+print(min3(4, 5, 5))  
+print(min3(4, 4, 4))  
+print(min3(-2, -6, -100))  
+print(min3("Z", "B", "A"))  
+
+print(min3(8, 9, 1))
+print(min3(9, 8, 2))
 
 # You should get this result:
 
