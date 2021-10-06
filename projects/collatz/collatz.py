c169a7c81958f6b0a7257a9a129af7f1bcbd70a6
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-10-06 09:21:05 +0000

    Collatz is finally working :)

--- projects/collatz/collatz.py
+++ projects/collatz/collatz.py
@@ -1,5 +1,6 @@
 
 def collatz(number):
+    print("Collatz sequence for", user_input)
     while number != 1:
         if number % 2 == 1:
             number = 3 * number + 1
@@ -11,29 +12,13 @@ def collatz(number):
             print("Enter a valid input.")
         continue
 
+user_input = input("Please enter an integer: ")
+user_input = collatz(int(user_input))
 
-
-
-collatz(3)
-
-#The Collatz Sequence  
-
-#Write a function named collatz() that has one parameter named number. If number is even, then collatz() should print number // 2 and return this value. If number is odd, then collatz() should print and return 3 * number + 1.
-
-#Then write code that lets the user type in an integer and that keeps calling collatz() on that number until the function returns the value 1. (Amazingly enough, this sequence actually works for any integer—sooner or later, using this sequence, you’ll arrive at 1! Even mathematicians aren’t sure why. Your program is exploring what’s called the Collatz sequence, sometimes called “the simplest impossible math problem.”)
-
-#Remember to convert the return value from input() to an integer with the int() function; otherwise, it will be a string value.
-
-#####################################
-
-# def user_input(number):
-
-
-# Uncomment this code to test your function
 
 '''The output of this program could look something like this:
 
-Enter number:  
+Enter number:  (if 3 is what is entered...)
 3  
 10  
 5  
